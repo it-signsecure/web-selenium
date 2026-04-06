9914b1e98ffd34bd6da0602f7c24cfcf4c2894a5
Harshal Desai <harshal.desai@kissht.com>
2026-04-06 19:59:02 +0530

    new changes

--- portfolio/src/components/TabBar.js
+++ portfolio/src/components/TabBar.js
@@ -1,5 +1,5 @@
 function TabBar({ activeTab, onTabChange }) {
-  const tabs = ["projects", "blogs", "news"];
+  const tabs = ["projects", "blogs", "Highlights"];
 
   return (
     <nav data-testid="tab-bar" className="tab-bar">
